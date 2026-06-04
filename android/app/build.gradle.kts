3306a66af586e760e85ad5ab3031dd11a71c4f17
Sandeep Sisodiya <sandeepsisodiya16092004@gmail.com>
2026-06-04 15:39:54 +0530

    last changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.ai_therapy"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     defaultConfig {
         applicationId = "com.example.ai_therapy"
